2cdaf5fc84fb840459b7073847e4b7353cfe8e52
Pierce13 <piercescott@weber.edu>
2024-05-15 20:14:31 -0600

    Initial Commit

---
+++ build/CMakeFiles/TestExample.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestExample.cpp.dir/TestExample.cpp.cpp.o"
+  "CMakeFiles/TestExample.cpp.dir/TestExample.cpp.cpp.o.d"
+  "libTestExample.cpp.a"
+  "libTestExample.cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestExample.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/piercescott/Weber State/Fall 2024/CS 1410/CPP-EXAMPLE
+# Build directory: /Users/piercescott/Weber State/Fall 2024/CS 1410/CPP-EXAMPLE/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
